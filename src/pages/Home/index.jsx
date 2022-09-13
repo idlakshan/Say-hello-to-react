bafbced981a36ac285a101cd783096691f500e38
idlakshan <id.lakshan21@gmail.com>
2022-09-13 10:34:06 +0530

    first step done

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,17 @@
+const {Component} = require("react");
+
+class HomePage extends Component{
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <div>
+                <h1>Hello From HomePage</h1>
+            </div>
+
+        )
+    }
+}
+
+export default HomePage
